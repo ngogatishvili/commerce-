6afd2af46bf3d70779897b370915a7b47b31eb86
ngogatishvili <106511724+ngogatishvili@users.noreply.github.com>
2022-07-18 09:22:11 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,39 @@
+
+const express=require('express');
+const app=express();
+const seedRouter=require("./routes/seedRoutes");
+const productRouter=require("./routes/productRoutes");
+const userRouter=require('./routes/userRoutes')
+
+const cors=require('cors')
+app.use(cors());
+const data=require("./data");
+const {StatusCodes}=require("http-status-codes")
+const dotenv=require('dotenv')
+const mongoose=require('mongoose');
+dotenv.config();
+  
+
+mongoose.connect(process.env.MONGODB_URI).then(()=>{
+    console.log('connected to DB')
+}).catch(err=>{
+        console.log(err.message);
+})
+   
+     
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.use("/seed",seedRouter)
+app.use("/products",productRouter)
+app.use('/users',userRouter);
+
+
+app.use((err,req,res,next)=>{
+    res.status(500).send({message:err.message})
+})
+
+
+
+app.listen(4000,console.log('app running at http://localhost:4000'))
